2e73e1f9b955f00249e739752ca89bc7ef2e436d
loddaoceania <murilocunha@alu.ufc.br>
2025-04-09 18:52:31 -0300

    att

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' // Use HashRouter instead of BrowserRouter
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
